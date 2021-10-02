30fbeff416ad78280cd1b5cd482502ac74095fd4
Sujal Singh Thakuri <sujalthakuribe@gmail.com>
2021-10-02 10:48:52 +0530

    Dashboard Features Added

--- app/src/main/java/com/example/nasaspaceapp2021/MainActivity.kt
+++ app/src/main/java/com/example/nasaspaceapp2021/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.nasaspaceapp2021
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.example.nasaspaceapp2021.ui.DashboardActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnContinue : Button
+    private lateinit var etName : EditText
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnContinue = findViewById(R.id.btnContinue)
+        etName = findViewById(R.id.etName)
+
+        btnContinue.setOnClickListener {
+            startActivity(Intent(this, DashboardActivity::class.java))
+        }
+
+
     }
 }
